153192836db0f8ed602a5cd18090ec18f571bcb0
DELL <DELL@DESKTOP-H3245V5>
2024-03-29 15:57:33 +0530

    Array

--- Java_Code/src/arrays/Day1.java
+++ Java_Code/src/arrays/Day1.java
@@ -4,7 +4,16 @@ public class Day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int arr[] = { 2, 5, 6, 9, 7, 4, 3 };
+		for(int i : arr) System.out.println("  : "+i);
+		for(int i =0;i<arr.length;i++) System.out.println("  new  at : "+i+" is : "+arr[i]);
 	}
 
+	/*
+	One-dimensional array (1-D arrays)
+	Two-dimensional (2D) array
+	Three-dimensional array	  
+	 */
+	
+	
 }
